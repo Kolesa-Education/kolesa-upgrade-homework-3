a7f314b8e38bb376d201c7336fde8812de14b326
Andrey <Andreyshabalin34@gmail.com>
2022-09-22 15:18:12 +0600

    Третье ДЗ

---
+++ webserv.go
@@ -0,0 +1,57 @@
+package main
+
+//С Go особо не знаком, поэтому возможно следующие стоки кода вызовут боль в глазах .-.
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Выбор категории изображений с котиком и вывод на экран
+func searchCatByCatId(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{} //инициализируем клиента
+	//Ввод в консоль значения для запроса
+	fmt.Println("Enter Cat Param: (from 1 to 15) ")
+	var category string
+	fmt.Scanln(&category)
+
+	/*
+		Задаём урл запроса, добавляем ему параметр, выполняем его, и закрываем тело ответа
+		для того чтобы избежать утечки памяти(в инете прочитал)
+	*/
+	req, err := http.NewRequest("GET", "https://api.thecatapi.com/v1/images/search", nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	q := req.URL.Query()
+	q.Add("category_ids", category)
+	req.URL.RawQuery = q.Encode()
+	fmt.Print(req)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Err is", err)
+	}
+	defer res.Body.Close()
+
+	//Считываем и парсим ответ
+	resBody, _ := ioutil.ReadAll(res.Body)
+	response := string(resBody)
+	resBytes := []byte(response)          // Конвертируем строку ответа в массив байтов (Я так понял в golang именно так работают с JSON)
+	var resArr []map[string]interface{}   // Объявляем словарь где ключи это строки а значения - интерфейсы(любой объект)
+	_ = json.Unmarshal(resBytes, &resArr) // Распаковываем результат в resArr
+	url := resArr[0]["url"].(string)
+	fmt.Fprintf(w, "<html><body><img src=\""+url+"\"/></body></html>")
+}
+
+func main() {
+	http.HandleFunc("/", searchCatByCatId)   // Устанавливаем роутер
+	err := http.ListenAndServe(":7777", nil) // устанавливаем порт веб-сервера
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
