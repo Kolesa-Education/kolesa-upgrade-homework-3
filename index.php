daa4159f18f8e6d83b8654b98ad60b58bf1994b7
Vladlen <vladli2000610@gmail.com>
2022-09-22 12:58:36 +0600

    Changed error message

--- index.php
+++ index.php
@@ -18,4 +18,4 @@ $content_json = json_decode($content->getBody()->getContents(), true)[0] ?? null
 
 $cat_url = $content_json['url'] ?? null;
 
-echo is_null($cat_url) ? "Wrong address" : template('template.php', ['cat_url' => $cat_url]);
+echo is_null($cat_url) ? "404 Not Found" : template('template.php', ['cat_url' => $cat_url]);
