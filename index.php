678dd32da167fbc433892dcdb9613e190ba2e2f8
UlanZhassanov <ulanzhasanov@gmail.com>
2022-09-22 15:17:02 +0600

    update + task2,3

--- index.php
+++ index.php
@@ -1,36 +1,62 @@
-<?php
+<html>
 
-use GuzzleHttp\Client;
+<body>
 
-require 'vendor/autoload.php';
+  <?php
 
-$client = new Client([
-  'base_uri' => 'https://api.thecaвtapi.com',
-  'timeout'  => 2.0,
-]);
+  use GuzzleHttp\Client;
+
+  require 'vendor/autoload.php';
+
+  $client = new Client([
+    'base_uri' => 'https://api.thecatapi.com',
+    'timeout'  => 2.0,
+  ]);
+  $category = $_GET['category_ids'] ?? null;
+
+  try {
+    echo  "<h5>Выберите категорию:</h5>";
+
+    $response = $client->request('GET', '/v1/categories');
+    $statuscode = $response->getStatusCode();
+    $responseBody = $response->getBody();
+    $contents = $responseBody->getContents();
+    $jsonContent = json_decode($contents, true);
+    foreach ($jsonContent as $jsonCategory) {
+      $url = $jsonCategory['name'];
+      echo  "<a href='".strtok($_SERVER["REQUEST_URI"], '?')."?category_ids=" . $jsonCategory['id'] . "'>" . $jsonCategory['name'] . "</a><br>";
+    }
+  } catch (GuzzleHttp\Exception\ClientException $e) {
+    echo $e->getResponse()->getStatusCode() . " " . $e->getResponse()->getReasonPhrase();
+  } catch (GuzzleHttp\Exception\ServerException $e) {
+    echo "Ошибка сервера";
+  } catch (\Exception $e) {
+    echo "Ошибка";
+  }
+
+  try {
+    if (!isset($category)) {
+      $response = $client->request('GET', '/v1/images/search');
+    } else {
+      $response = $client->request('GET', '/v1/images/search?category_ids=' . $category);
+    }
+    $statuscode = $response->getStatusCode();
+
+    $responseBody = $response->getBody();
+    $contents = $responseBody->getContents();
+    $jsonContent = json_decode($contents, true);
+    $url = $jsonContent[0]['url'];
+    echo '<img src=' . $url . ' alt="random_cat" style="max-width:300px; max-height:300px">';
+  } catch (GuzzleHttp\Exception\ClientException $e) {
+    echo $e->getResponse()->getStatusCode() . " " . $e->getResponse()->getReasonPhrase();
+  } catch (GuzzleHttp\Exception\ServerException $e) {
+    echo "Ошибка сервера";
+  } catch (\Exception $e) {
+    echo "Ошибка";
+  }
 
 
-try {
-  $response = $client->request('GET', '/v1/images/search/');
-  $statuscode = $response->getStatusCode();
-  
-  $responseBody = $response->getBody();
-  $contents = $responseBody->getContents();
-  $jsonContent = json_decode($contents, true);
-  $url = $jsonContent[0]['url'];
-  echo '
-  <html>
-    <body>
-      <img src=' . $url . ' alt="random_cat" style="max-width:300px; max-height:300px">
-    </body>
-  </html>
-  ';
-} catch (GuzzleHttp\Exception\ClientException $e) {
-  echo $e->getResponse()->getStatusCode()." ".$e->getResponse()->getReasonPhrase();
-} catch (GuzzleHttp\Exception\ServerException $e) {
-  echo "Ошибка сервера";
-}  catch (\Exception $e) {
-  echo "Ошибка";
-}
+  ?>
 
-// print_r(http_response_code(404));
+</html>
+</body>
