ad8c60d979914cfb27334e68d494c8790330fbb0
FishSev <ilya.sevrugin@gmail.com>
2022-09-23 16:14:17 +0600

    1) исправил код согласно замечаниям; 2) отработал все рекомендации.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$autoloadPath = __DIR__ . '/vendor/autoload.php';
+require_once $autoloadPath;
+require_once 'src\CatPictureService.php';
+
+use cat\CatPictureService as Cat;
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Страничка с котиками</title>
+    <link rel="stylesheet" href="src\style.css">
+</head>
+
+<body>
+
+    Здесь показывают котиков.<br>
+    Категорию можно выбрать из списка добавив <b>?category_ids=</b><br>
+
+    <?php
+    $cat = new Cat();
+    $cat->initializeClient();
+    $cat->outputCategoryList();
+    $cat->outputCat();
+    ?>
+
+</body>
+
+</html>
