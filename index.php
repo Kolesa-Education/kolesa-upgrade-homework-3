b359ffb10c631dc520652125054f7ce0e049cc26
Zhass <zhass.akimbayev@gmail.com>
2022-09-25 10:59:00 +0600

    выполняю задание 3

--- index.php
+++ index.php
@@ -1,76 +1,35 @@
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
-        <title>Sayat</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Я котик,ты котик</title>
+</head>
 
-    <body class="vsc-initialized">
-        <main role="main">
-            <section class="jumbotron text-center">
-                <div class="container">
-                    <h1 class="jumbotron-heading">Cats</h1>
-                    <p class="lead text-muted">Милые, красивые, разные котики</p>
-                </div>
-            </section>
+<body>
+<?php
 
-            <form class="needs-validation" action="index.php" method="POST" novalidate>
-                    <div class="row">
-                        <div class="col-md-3 mb-1"></div>
+require __DIR__ . '/vendor/autoload.php';
 
-                        <div class="col-md-2 mb-1">
-                            <label for="num1">Количество котиков</label>
-                            <input type="number" class="form-control" name="limit" min="1" max="10">
-                        </div>
 
-                        <div class="col-md-2 mb-1">
-                            <label for="num1">Номер страницы</label>
-                            <input type="number" class="form-control" name="page">
-                        </div>
+$url = file_get_contents("https://api.thecatapi.com/v1/images/search");
 
-                        <div class="col-md-2 mb-1">
-                            <label for="num2">По:</label>
-                            <select class="form-control" id="exampleFormControlSelect2" name="order">
-                                <option value="Desc">возрастанию</option>
-                                <option value="Asc">убыванию</option>
-                            </select>
-                        </div>
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_URL, 'https://api.thecatapi.com/v1/images/search');
+curl_setopt($curl, CURLOPT_REFERER, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 
-                        <div class="col-md-3 mb-1"></div>
-                    </div>
 
-                    <div class="row">
-                        <div class="col-md-5 mb-1"></div>
+$parsed = curl_exec($curl);
+curl_close($curl);
 
-                        <button class="btn btn-primary col-md-2 m-1" name="submit" type="submit">Показать</button>
+$arrayData = json_decode($parsed, true);
+// echo $arrayData[0]['url'];
+?>
+<img src="<?php echo $arrayData[0]['url']; ?>" alt="Cat" />
+</body>
 
-                        <div class="col-md-5 mb-1"></div>
-                    </div>
-
-                </form>
-
-            <div class="album py-5 bg-light">
-                <div class="container">
-                    <div class="row">
-
-                        <?php require 'cat.php'; ?>
-
-                    </div>
-                </div>
-            </div>
-        </main>
-
-        <footer class="text-muted">
-            <div class="container">
-                <p class="float-right">
-                    <a href="#">Вверх</a>
-                </p>
-            </div>
-        </footer>
-    </body>
 </html>
