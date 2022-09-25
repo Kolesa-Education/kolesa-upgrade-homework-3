ad5f9892447c0efa690d0851425b4a8622479c62
MyrzakhmetSmagul <smagul.myrzakhmet@mail.ru>
2022-09-25 14:09:20 +0600

    вывел котиков на экран, изменил обработку ошибок

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -32,65 +31,64 @@ func main() {
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
-		fmt.Println("r.Method != http.MethodGet")
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 	if r.URL.Path != "/" {
-		fmt.Println("r.URL.Path != \"/\"", r.URL.Path)
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
 	t, err := template.ParseFiles("./static/index.html")
 	if err != nil {
-		fmt.Println("template.ParseFiles(\"./static/index.html\")", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	err = t.Execute(w, nil)
 	if err != nil {
-		fmt.Println("t.ExecuteTemplate(w, \"index\", nil)", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 }
 
 func CatHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("cats post", r.URL.Path)
-
+	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
 	t, err := template.ParseFiles("./static/index.html")
 	if err != nil {
-		fmt.Println("template doesn't parse in catHandler", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	err = r.ParseForm()
 	if err != nil {
-		fmt.Println("parse form fail", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	limit := r.PostFormValue("limit")
 	page := r.PostFormValue("page")
 	order := r.PostFormValue("sort")
-	fmt.Println(limit, page, order)
 	requestUrl := URL + "?limit=" + limit + "&page=" + page + "&order=" + order
-	Cats, err := GetJson(requestUrl)
+	Cats, err := getJson(requestUrl)
 	if err != nil {
-		fmt.Println("parse Json fail", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	err = t.Execute(w, Cats)
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 }
 
-func GetJson(url string) ([]Cat, error) {
-	fmt.Println("url", url)
-	r, err := myClient.Get(url)
+func getJson(url string) ([]Cat, error) {
 	var Cats []Cat
+	r, err := myClient.Get(url)
 	if err != nil {
 		return Cats, err
 	}
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
-	fmt.Printf("%v\n", string(body))
 	err = json.Unmarshal(body, &Cats)
 	return Cats, err
 }
