2b27850d054c99b765da4118bfbaa3aeab8f7174
Kanatov Nurzhas <nurzhaskanatovbarca@gmail.com>
2022-09-25 14:22:41 +0600

    first

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cats((</title>
+</head>
+<body>
 <?php
 
-print_r('Proverka,Proverka');
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'base_uri' => 'https://api.thecatapi.com/',
+    'timeout' => 2.0,
+]);
+
+$response = $client->get('v1/images/search');
+
+echo '<pre>';
+$result = json_decode($response->getBody()->getContents(),true);
+echo '<img src="' . $result[0]['url'] . '">';
+?>
+</body>
+</html>
