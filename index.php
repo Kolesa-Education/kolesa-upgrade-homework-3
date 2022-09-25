c461651b8b847a4b12754acf15bb2dd606dc83d2
Nazar Uakhitov <uakhitov97@gmail.com>
2022-09-25 21:13:47 +0600

    выполняю задание 3

--- index.php
+++ index.php
@@ -1,76 +1,51 @@
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-        <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
-        <title>Sayat</title>
-    </head>
-
-    <body class="vsc-initialized">
-        <main role="main">
-            <section class="jumbotron text-center">
-                <div class="container">
-                    <h1 class="jumbotron-heading">Cats</h1>
-                    <p class="lead text-muted">Милые, красивые, разные котики</p>
-                </div>
-            </section>
-
-            <form class="needs-validation" action="index.php" method="POST" novalidate>
-                    <div class="row">
-                        <div class="col-md-3 mb-1"></div>
-
-                        <div class="col-md-2 mb-1">
-                            <label for="num1">Количество котиков</label>
-                            <input type="number" class="form-control" name="limit" min="1" max="10">
-                        </div>
-
-                        <div class="col-md-2 mb-1">
-                            <label for="num1">Номер страницы</label>
-                            <input type="number" class="form-control" name="page">
-                        </div>
-
-                        <div class="col-md-2 mb-1">
-                            <label for="num2">По:</label>
-                            <select class="form-control" id="exampleFormControlSelect2" name="order">
-                                <option value="Desc">возрастанию</option>
-                                <option value="Asc">убыванию</option>
-                            </select>
-                        </div>
-
-                        <div class="col-md-3 mb-1"></div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-5 mb-1"></div>
-
-                        <button class="btn btn-primary col-md-2 m-1" name="submit" type="submit">Показать</button>
-
-                        <div class="col-md-5 mb-1"></div>
-                    </div>
-
-                </form>
-
-            <div class="album py-5 bg-light">
-                <div class="container">
-                    <div class="row">
-
-                        <?php require 'cat.php'; ?>
-
-                    </div>
-                </div>
-            </div>
-        </main>
-
-        <footer class="text-muted">
-            <div class="container">
-                <p class="float-right">
-                    <a href="#">Вверх</a>
-                </p>
-            </div>
-        </footer>
-    </body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 3 - Nazar Uakhitov</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+  </head>
+  <body>
+    <h1>Search for a cat photo</h1>
+    <form action="index.php" method="POST">
+      <div class="mb-3">
+        <label for="category" class="form-label">Choose a category you are keen on: </label>
+        <select name="category" aria-label="Default select example">
+          <option name="category" value="1">Hats</option>
+          <option name="category" value="2">Space</option>
+          <option name="category" value="3">Funny</option>
+          <option name="category" value="4">Sunglasses</option>
+          <option name="category" value="5">Boxes</option>
+          <option name="category" value="6">Caturday</option>
+          <option name="category" value="7">Ties</option>
+          <option name="category" value="9">Dream</option>
+          <option name="category" value="10">Kittens</option>
+          <option name="category" value="14">Sinks</option>
+          <option name="category" value="15">Clothes</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="inputAmount">How many photos you want to see?</label>
+        <input class="formControl" id="inputAmount" placeholder="Enter integer number" name="amount">
+      </div>
+      <button type="submit" class="btn btn-primary" name="find">Search</button>
+    </form>
+    <div class="mt-2">
+      <?php
+      if (isset($_POST['find'])) {
+        $amount = $_POST['amount'];
+        require 'get_cat_image_url.php';
+        for ($i=0; $i < $amount; $i++) { 
+          $url = get_url($_POST);
+          ?>
+          <img src="<?php echo $url ?>" height="300" alt="">
+    </div>
+    <?php
+    }
+    }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
 </html>
