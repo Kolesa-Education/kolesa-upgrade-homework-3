e9342274f006fae2c055c36e977fb6791ef5202d
Evgeniy Smolnikov <evg.v.sml@gmail.com>
2022-09-25 19:04:20 +0600

    Выполняю задание 3

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+// Проверить работу можно тут - https://sitecodera.ru/demo/cats
+
+require_once  __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client as Client;
+
+$httpClient = new Client(['base_uri'=>'https://api.thecatapi.com/v1/images/search?limit=3']);
+$response = $httpClient->request('GET');
+
+
+$contents = $response->getBody();
+$jsonContent = json_decode($contents, true);
+//var_dump($jsonContent);
+$url = $jsonContent[0]['url'];
+
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Smolnikov</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<h1>Cats</h1>
+<form action="">
+<input type="submit" value="Обновить">
+</form>
+<section>
+<img src="<?=$url?>" style="max-width:600px; max-height:600px">
+</section>
+
+</body>
+</html>
