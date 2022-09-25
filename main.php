6f2d36b3e87333f36108f4f817602566d14f754d
Askar <askxrqq@gmail.com>
2022-09-25 23:24:03 +0600

    cat api

--- main.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$urlstring = "https://api.thecatapi.com/v1/breeds/search?q=" . str_replace(' ', '%20', $_GET["name"]);
-$data = file_get_contents($urlstring); 
-$dataj = json_decode($data);
