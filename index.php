fcd9365da93c13b972427a2f500f247bb2a49a7b
Kanatov Nurzhas <nurzhaskanatovbarca@gmail.com>
2022-09-25 14:25:39 +0600

    delete m.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cats((</title>
-</head>
-<body>
 <?php
 
 require 'vendor/autoload.php';
@@ -24,5 +15,16 @@ echo '<pre>';
 $result = json_decode($response->getBody()->getContents(),true);
 echo '<img src="' . $result[0]['url'] . '">';
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cats((</title>
+</head>
+<body>
 </body>
 </html>
