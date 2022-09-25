1b3a20349674062ab1105b1b9900327beffbe1aa
Vassiliy Vlassenko <vlasenko@kolesa.kz>
2022-09-25 22:44:56 +0600

    cat is done

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+require 'vendor/autoload.php';
+use GuzzleHttp\Client as Client;
 
-require 'vendor/autoload.php'
 
+$client = new Client([
+    'base_uri' => 'https://api.thecatapi.com/v1/images/',
+    'timeout'  => '2.0'
+]);
+
+$response = $client->get('search');
+$getBody = json_decode($response->getBody());
+$getUrlFromArray = $getBody[0];
+$url = $getUrlFromArray->url;
+
+//print_r($url);
+
+echo <<<HTML
+<html><body><img src=$url/></body></html>
+HTML;
+
+?>
