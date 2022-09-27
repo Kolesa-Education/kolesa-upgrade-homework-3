6ea3c06010e6dc4e31a14d4fffbbd00c6a9c971e
Omargaly Bitebayev <hackeevo@gmail.com>
2022-09-27 16:51:09 +0600

    делаю изменения по заданию 3

--- Service.php
+++ Service.php
@@ -1,32 +1,43 @@
 <?php
 
-use GuzzleHttp\Client;
-
 class Service
 {
     private $client;
+    private $category;
 
-    public function serviceInitialization() {
-        $client = new Client([
-            'base_uri' => 'https://api.thecatapi.com/',
-            'timeout' => 2.0,
-            'verify' => false,
-        ]);
-
+    public function __construct($client, $category) {
         $this->client = $client;
+        $this->category = $category;
     }
 
-    public function getCategory() {
+    public function getCategories(): void
+    {
         $response = $this->client->get(uri: 'v1/categories');
         $array = json_decode($response->getBody()->getContents(), associative: true);
         asort($array);
         foreach ($array as $category) {
-            echo '<tr> <th>' . $category["id"] . ' </th> <th>' . $category["name"] . ' </th> </tr> ' . PHP_EOL;
+            echo '<tr> <th> <a href="index.php?category_ids=' . $category["id"] . '">' . $category["id"] . '</a> </th> <th> <a href="index.php?category_ids=' . $category["id"] . '">' . $category["name"] . '</a> </th> </tr> ' . PHP_EOL;
         }
     }
 
-    public function getPicture() {
-        $category = $_GET['category_ids'] ?? null;
+    public function getCategoryName($category): void
+    {
+        $this->category = $category;
+        $response = $this->client->get(uri: 'v1/categories');
+        $array = json_decode($response->getBody()->getContents(), associative: true);
+        asort($array);
+        if (!isset($category)) {
+            echo '<h2>Current category is not choosen.<h2>' . PHP_EOL;
+        }
+        foreach ($array as $element) {
+            if ($category == $element["id"]) {
+                echo '<h2>Current category name is "' . $element["name"] . '".<h2>' . PHP_EOL;
+            }
+        }
+    }
+
+    public function getPicture($category): void
+    {
         if (!isset($category)) {
             $additionalAddress = 'v1/images/search';
         } else {
@@ -38,7 +49,7 @@ class Service
             if (empty($array)) {
                 echo "<h1>Error! Incorrect category has been choosing, try another one.<h1>";
             } else {
-                echo '<img src="' . $array[0]['url'] . '" width="800" height="600"/>';
+                echo '<img src="' . $array[0]['url'] . '"/>';
             }
         } catch (\Throwable $e) {
             echo $e->getMessage();
--- index.php
+++ index.php
@@ -3,31 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <title>Get your "CatPic" here!</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <br>
     <h2>Here, you see the random cat picture, refresh this page to get another one.</h2>
     <br>
-    Use categories for filter by adding <b>?category_ids=</b> to the address link.<br>
+    Use categories for filter by clicking to the category name or type <b>?category_ids=</b> to the address link.<br>
     <br>
 <?php
-$autoloadPath = __DIR__ . '/vendor/autoload.php';
 
-require_once $autoloadPath;
+require_once __DIR__ . '/vendor/autoload.php';
 
-$catService = new Service();
-$catService->serviceInitialization();
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'base_uri' => 'https://api.thecatapi.com/',
+    'timeout' => 2.0,
+    'verify' => false,
+]);
+$category = $_GET['category_ids'] ?? null;
+$catService = new Service($client, $category);
+$catService->getCategoryName($category);
 ?>
+    <div class="container">
+        <div>
     <table>
 <?php
-$catService->getCategory();
+$catService->getCategories();
 ?>
     </table>
-    <br>
-    <div align="center">
+        </div>
+        <div>
 <?php
-$catService->getPicture();
+$catService->getPicture($category);
 ?>
+    </div>
     </div>
     </body>
     </html>
